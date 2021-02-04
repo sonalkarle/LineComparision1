310aaca1c6003234c47fed6f713928ea93a2b31f
sonal <sonalkarle01@gmail.com>
2021-02-04 18:07:10 +0530

    Check two length are equal or not using equal method

--- LineComparision1/LineComparision1/Program.cs
+++ LineComparision1/LineComparision1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace LineComparision
 {
@@ -6,21 +7,22 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the value of x1");    // Take x1 value from user
-            int x1 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
-            Console.WriteLine("x1 = " + x1);
-            Console.WriteLine("Enter the value of x2");    // Take x2 value from user
-            int x2 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
-            Console.WriteLine("x2 = " + x2);
-            Console.WriteLine("Enter the value of y1");    // Take y1 value from user
-            int y1 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
-            Console.WriteLine("y1 = " + y1);
-            Console.WriteLine("Enter the value of y2");    // Take y2 value from user
-            int y2 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
-            Console.WriteLine("y2 = " + y2);
-            int a = (((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
-            Console.WriteLine("Length of line" + (Math.Sqrt(a)));
 
+            Console.WriteLine("Enter the end point of first line");     //Taking input from user
+            int x2 = Convert.ToInt32(Console.ReadLine());               //Convert input in integer format
+            Console.WriteLine("x2 = " + x2);                            //Display input 
+            Console.WriteLine("Enter the end point of second line");    //Convert input in integer format
+            int y2 = Convert.ToInt32(Console.ReadLine());               //Display input
+            Console.WriteLine("y2 = " + y2);
+
+            if (x2.Equals(y2))                        //computation
+            {
+                Console.WriteLine("First line is equal to second line ");
+            }
+            else
+            {
+                Console.WriteLine("First line is not equal to second line");
+            }
 
         }
     }
