847a7700d3b4d7464761e0365e9606480807bc57
sonal <sonalkarle01@gmail.com>
2021-02-04 17:45:14 +0530

    Find length of line using cartesian methif

--- LineComparision1/LineComparision1/Program.cs
+++ LineComparision1/LineComparision1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace LineComparision1
+namespace LineComparision
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome  to line comaprision computation");
+            Console.WriteLine("Enter the value of x1");    // Take x1 value from user
+            int x1 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("Enter the value of x2");    // Take x2 value from user
+            int x2 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
+            Console.WriteLine("x2 = " + x2);
+            Console.WriteLine("Enter the value of y1");    // Take y1 value from user
+            int y1 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
+            Console.WriteLine("y1 = " + y1);
+            Console.WriteLine("Enter the value of y2");    // Take y2 value from user
+            int y2 = Convert.ToInt32(Console.ReadLine());  //Convert into integer
+            Console.WriteLine("y2 = " + y2);
+            int a = (((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+            Console.WriteLine("Length of line" + (Math.Sqrt(a)));
+
+
         }
     }
 }
