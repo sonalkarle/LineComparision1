5425fcfc4c9b8ad9715b483b8f7b59c0da232be9
sonal <sonalkarle01@gmail.com>
2021-02-05 12:02:11 +0530

    Check line by compare to method to check greater or lesser

--- LineComparision1/LineComparision1/Program.cs
+++ LineComparision1/LineComparision1/Program.cs
@@ -8,17 +8,17 @@ namespace LineComparision
         {
 
             Console.WriteLine("Enter the end point of first line");    // Taking input from user
-            int x2 = Convert.ToInt32(Console.ReadLine());              //Conversion
-            Console.WriteLine("x2 = " + x2);                           //Display the input
+            int l2 = Convert.ToInt32(Console.ReadLine());              //Conversion
+            Console.WriteLine("l2 = " + l2);                           //Display the input
             Console.WriteLine("Enter the end point of second line");   //Taking input from user
-            int y2 = Convert.ToInt32(Console.ReadLine());              //Conversion
-            Console.WriteLine("y2 = " + y2);                          //Display the input
+            int b2 = Convert.ToInt32(Console.ReadLine());              //Conversion
+            Console.WriteLine("b2 = " + b2);                          //Display the input
 
-            if (x2.CompareTo(y2) == 0)                                   //Compuatation
+            if (l2.CompareTo(b2) == 0)                                   //Compuatation
             {
                 Console.WriteLine("First line is equal to second line ");
             }
-            else if (x2 > y2)                                           //Computation
+            else if (l2 > b2)                                           //Computation
             {
                 Console.WriteLine("First line is greater than second line");
             }
