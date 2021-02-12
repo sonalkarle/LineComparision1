bc764a6ed703dad77b794750c4aaca8f0f2c6030
sonal <sonalkarle01@gmail.com>
2021-02-12 16:12:36 +0530

    Given message ability to analyse the respondsHppy or Sad mood

---
+++ MoodAnalyser/MoodanalyserDay10/MoodAnalyser1/MoodAnalyser1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MoodAnalyserOfPerson
+{
+    public class MoodAnalyser
+    {
+        string Mood;
+
+        public MoodAnalyser()
+        {
+        }
+
+        public MoodAnalyser(string Mood)
+        {
+            this.Mood = Mood;
+        }
+
+        public string AnalyseMood()
+        {
+            if (Mood.ToLower().Contains("sad"))
+                return "SAD";
+            else return "HAPPY";
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
